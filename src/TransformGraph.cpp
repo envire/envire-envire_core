1fe7a087ab3c66f360907f07a4e81a7d3044df18
Arne Böckmann <arne.boeckmann@dfki.de>
2015-09-24 11:04:32 +0200

    Forgot to add 2 files

---
+++ src/TransformGraph.cpp
@@ -0,0 +1,208 @@
+/*
+ * TransformTree.cpp
+ *
+ *  Created on: Sep 21, 2015
+ *      Author: aboeckmann
+ */
+#include "TransformGraph.hpp"
+
+using namespace envire::core;
+using edgePair = std::pair<edge_descriptor, bool>;
+
+void TransformGraph::addTransform(const FrameId& origin, const FrameId& target,
+                                 const Transform& tf)
+{
+    //try to find vertices
+    vertex_descriptor originDesc = vertex(origin);
+    vertex_descriptor targetDesc = vertex(target);
+    //if they don't exist create them
+    if(originDesc == null_vertex())
+        originDesc = add_vertex(origin);
+    if(targetDesc == null_vertex())
+        targetDesc = add_vertex(target);
+
+    //check if there is an edge between the vertices.
+    //If a->b exists, b->a also exist. Therefore we need to check only one direction
+    edgePair e = boost::edge(originDesc, targetDesc, *this);
+    if(e.second)// edge already exists
+    {
+        throw TransformAlreadyExistsException(origin, target);
+    }
+    //we always add two edges, one for the transform and one for the inverse transform
+    edge_descriptor originToTarget = add_edge(originDesc, targetDesc, tf);
+
+    Transform invTf = tf;
+    invTf.setTransform(invTf.transform.inverse());
+    edge_descriptor targetToOrigin = add_edge(targetDesc, originDesc, invTf);
+}
+
+const Transform& TransformGraph::getTransform(const FrameId& a, const FrameId& b) const
+{
+    //calling boost::edge_by_label on an empty graph results in a segfault.
+    //therefore catch it before that happens.
+    if(num_edges() == 0)
+    {
+        throw UnknownTransformException(a, b);
+    }
+    //FIXME for now this only works with direct edges
+    edgePair pair = boost::edge_by_label(a, b, *this);
+    if(!pair.second)
+    {
+        throw UnknownTransformException(a, b);
+    }
+    return (*this)[pair.first].transform;
+}
+
+void TransformGraph::updateTransform(const FrameId& origin, const FrameId& target,
+                                    const Transform& tf)
+{
+    if(num_edges() <= 0)
+    {   //boost::edge_by_label segfaults on empty graphs...
+        throw UnknownTransformException(origin, target);
+    }
+
+    edgePair originToTarget = boost::edge_by_label(origin, target, *this);
+    if(!originToTarget.second)
+    {
+      throw UnknownTransformException(origin, target);
+    }
+
+    updateTransform(originToTarget.first, tf, origin, target);
+    Transform invTf = tf;//copies the time
+    invTf.setTransform(tf.transform.inverse());
+    edgePair targetToOrigin = boost::edge_by_label(target, origin, *this);
+    assert(targetToOrigin.second);//there should always be an inverse edge
+    updateTransform(targetToOrigin.first, invTf, target, origin);
+}
+
+void TransformGraph::removeTransform(const FrameId& origin, const FrameId& target)
+{
+    //note: do not use boost::edge_by_label as it will segfault if one of the
+    //frames is not part of the tree.
+
+    vertex_descriptor originDesc = vertex(origin);
+    vertex_descriptor targetDesc = vertex(target);
+    if(originDesc == null_vertex() || targetDesc == null_vertex())
+    {
+        throw UnknownTransformException(origin, target);
+    }
+
+    edgePair originToTarget = boost::edge(originDesc, targetDesc, graph());
+    edgePair targetToOrigin = boost::edge(targetDesc, originDesc, graph());
+    if(!originToTarget.second || !targetToOrigin.second)
+    {
+        throw UnknownTransformException(origin, target);
+    }
+    boost::remove_edge(originToTarget.first, *this);
+    notify(TransformRemovedEvent(origin, target));
+    boost::remove_edge(targetToOrigin.first, *this);
+    notify(TransformRemovedEvent(target, origin));
+
+    //remove dangling frames
+    if(boost::degree(vertex(origin), *this) <= 0)
+    {
+        remove_frame(origin);
+    }
+    if(boost::degree(vertex(target), *this) <= 0)
+    {
+        remove_frame(target);
+    }
+}
+
+edge_descriptor TransformGraph::add_edge(const vertex_descriptor node_from,
+                                        const vertex_descriptor node_to,
+                                        const envire::core::Transform &tf)
+{
+    TransformProperty tf_prop;
+    tf_prop.transform = tf;
+    auto edge_pair =  boost::add_edge(node_from, node_to, tf_prop, *this);
+    notify(TransformAddedEvent(node_from, node_to, edge_pair.first, tf));
+    return edge_pair.first;
+}
+
+vertices_size_type TransformGraph::num_vertices() const
+{
+    return boost::num_vertices(*this);
+}
+
+edges_size_type TransformGraph::num_edges() const
+{
+    return boost::num_edges(*this);
+}
+
+vertex_descriptor TransformGraph::add_vertex(const FrameId& frameId)
+{
+    FrameProperty node_prop;
+    vertex_descriptor v = LabeledTransformGraph::add_vertex(frameId, node_prop);
+    return v;
+}
+
+void TransformGraph::remove_frame(FrameId fId)
+{
+    assert(boost::degree(vertex(fId), *this) <= 0);
+    boost::remove_vertex(fId, *this);
+    //HACK this is a workaround for bug https://svn.boost.org/trac/boost/ticket/9493
+    //It should be removed as soon as the bug is fixed in boost.
+    //If the bug is fixed also remove the #define private protected in TransformTreeTypes
+    map_type::iterator it = _map.find(fId);
+    if(it != _map.end())
+        _map.erase(it);
+}
+
+const envire::core::Frame& TransformGraph::getFrame(const FrameId& frame)
+{
+    if(vertex(frame) == null_vertex())
+    {
+        throw UnknownFrameException(frame);
+    }
+    return (*this)[frame].frame;
+}
+
+edge_descriptor TransformGraph::getEdge(const FrameId& origin, const FrameId& target) const
+{
+    vertex_descriptor originDesc = vertex(origin);
+    vertex_descriptor targetDesc = vertex(target);
+
+    if(originDesc == null_vertex() || targetDesc == null_vertex())
+    {//boost segfaults if one of them is null
+        throw UnknownTransformException(origin, target);
+    }
+
+    edgePair e = boost::edge(originDesc, targetDesc, graph());
+    if(!e.second)
+    {
+        throw UnknownTransformException(origin, target);
+    }
+    return e.first;
+}
+
+void TransformGraph::updateTransform(edge_descriptor ed, const Transform& tf,
+                                    const FrameId& origin, const FrameId& target)
+{
+    Transform old = (*this)[ed].transform;
+    boost::put(&TransformProperty::transform, *this, ed, tf);
+    notify(TransformModifiedEvent(origin, target, ed, old, (*this)[ed].transform));
+}
+
+void TransformGraph::addItemToFrame(const FrameId& frame, boost::intrusive_ptr<ItemBase> item)
+{
+    if(vertex(frame) == null_vertex())
+    {
+        throw UnknownFrameException(frame);
+    }
+    
+    (*this)[frame].frame.items.push_back(item);
+    
+}
+
+const std::vector<boost::intrusive_ptr<ItemBase>>& TransformGraph::getItems(const FrameId& frame) const
+{
+    //FIXME if item events should be added later this method needs to change because it provides a way
+    //to modify items without the event system noticing
+    if(vertex(frame) == null_vertex())
+    {
+        throw UnknownFrameException(frame);
+    }
+    return (*this)[frame].frame.items;
+}
+
---
+++ src/TransformGraph.hpp
@@ -0,0 +1,151 @@
+/**\file TransformTree.hpp
+ *
+ * This class provided stores and manages the transformation tree
+ *
+ * @author Javier Hidalgo Carrio et. al (see THANKS  for the full Author list)
+ * See LICENSE for the license information
+ *
+ */
+
+
+#ifndef __ENVIRE_CORE_TRANSFORM_TREE__
+#define __ENVIRE_CORE_TRANSFORM_TREE__
+
+
+#include <boost/uuid/uuid.hpp>
+#include <cassert>
+#include <string>
+
+
+#include "TransformGraphTypes.hpp"
+#include "TransformTreeExceptions.hpp"
+#include "events/GraphEventPublisher.hpp"
+#include "events/TransformAddedEvent.hpp"
+#include "events/TransformRemovedEvent.hpp"
+#include "events/TransformModifiedEvent.hpp"
+
+namespace envire { namespace core
+{
+    class GraphViz;
+    /**
+     * A tree-like graph structure.
+     * Each vertex contains a labeled Frame. The label must be unique.
+     *
+     * @note The inheritance from TransformGraph is protected to stop
+     *       users from calling boost::graph methods directly.
+     *       This way the tree methods are the only way to manipulate
+     *       the vertices and edges.
+     *       This is done to ensure that
+    */
+    class TransformGraph : protected LabeledTransformGraph, public GraphEventPublisher
+    {
+        //GraphViz has been implemented for TransformGraph.
+        //As it only reads and does no manipulation it is ok to let it know
+        //about the inheritance from TransformGraph
+        friend class GraphViz;
+    public:
+
+        /***************************************************
+         * Methods Naming convention
+         * Overloading boost methods uses delimited separated
+         * words, new methods use Camel Case separated words
+         ***************************************************/
+
+        TransformGraph(envire::core::Environment const &environment = Environment()):
+          LabeledTransformGraph (environment)
+        {}
+
+
+        /**Adds a transform from frame @p a to frame @p b.
+         * If the frames do not exist, they will be created.
+         * If the transform already exists, it will **not** be updated.
+         *
+         * Causes a TransformAdded event.
+         *
+         * The inverse transform will be added automatically.
+         *
+         * @throw TransformAlreadyExistsException if the transformation already exists.*/
+        void addTransform(const FrameId& origin, const FrameId& target,
+                          const Transform& tf);
+
+        /**Updates the value of the transform from @p origin to
+         * @p target and the inverse transformation according to @p tf.
+         * Both frames need to exist beforehand.
+         * A direct transformation has to exist between @p orign and @p target
+         * for this method to work.
+         *
+         * Causes two TransformUpdated events. One for the
+         * transform and one for the inverse.
+         *
+         * @throw UnknownTransformException if no direct transformation between
+         *        @p origin and @p target exists. */
+        void updateTransform(const FrameId& origin, const FrameId& target,
+                             const Transform& tf);
+
+        /**Removes the transform between @p origin and @p target as well as
+         * the transform between @p target and @p origin.
+         * If frames are left unconnected after the transform has been removed, they
+         * will be removed as well.
+         *
+         * Causes TransformRemoved event.
+         *
+         * @throw UnknownTransformException if the transformation doesn't exist */
+        void removeTransform(const FrameId& origin, const FrameId& target);
+
+        /** @return the transform between a and b. Calculating it if necessary.
+         * @throw UnknownTransformException if the transformation doesn't exist*/
+        const Transform& getTransform(const FrameId& a, const FrameId& b) const;
+
+        /** @return the edge between frame @p origin and @p target
+         * @throw UnknownTransformException if there is no such edge  */
+        edge_descriptor getEdge(const FrameId& origin, const FrameId& target) const;
+
+        /** @return a reference to the frame identified by the id.
+         *  @throw UnknownFrameException if the frame id is invalid **/
+        const envire::core::Frame& getFrame(const FrameId& frame);
+        
+        /** Adds @p item to the item list of the specified frame 
+         *  @throw UnknownFrameException if the frame id is invalid **/
+        void addItemToFrame(const FrameId& frame, boost::intrusive_ptr<ItemBase> item);
+        
+        /** @return a list of items that are attached to the specified @p frame.
+         *  @throw UnknownFrameException if the @p frame id is invalid.*/
+        const std::vector<boost::intrusive_ptr<ItemBase>>& getItems(const FrameId& frame) const;
+        
+
+        vertices_size_type num_vertices() const;
+        edges_size_type num_edges() const;
+
+    protected:
+         /**@brief Add a vertex
+         * @note the frame's name must be unique. */
+        vertex_descriptor add_vertex(const FrameId& frameId);
+
+        /**@brief Add an Edge
+         * Add an edge between two vertices.
+         *
+         * Causes TransformAddedEvent or TransformModifiedEvent.
+         *
+         * @return an edge descriptor pointing to the new edge and a boolean.
+         *         The boolean is false if the edge already existed.
+         *         In that case no new edge was added, instead the existing
+         *         edge was updated.
+         */
+        edge_descriptor add_edge(const vertex_descriptor node_from,
+                                 const vertex_descriptor node_to,
+                                 const envire::core::Transform &tf);
+
+        /**Removes a vertex from the tree.
+         * A vertex can only be removed if there are no edges to
+         * and from the vertex. Removing a vertex that still has edges
+         * attached will result in undefined behavior. */
+        void remove_frame(FrameId fId);
+
+        /**Sets the transform value and causes transformModified event. */
+        void updateTransform(edge_descriptor ed, const Transform& tf,
+                             const FrameId& origin, const FrameId& target);
+
+
+    };
+}}
+#endif
