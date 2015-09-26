e2fe68c05faac01fd15c677696cf65219b08a0e7
Javier Hidalgo <javier.hidalgo_carrio@dfki.de>
2015-09-26 18:20:52 +0200

    Visitors class for the BGL search algorithms

--- src/TransformGraph.hpp
+++ src/TransformGraph.hpp
@@ -92,7 +92,7 @@ namespace envire { namespace core
 
         /** @return the transform between a and b. Calculating it if necessary.
          * @throw UnknownTransformException if the transformation doesn't exist*/
-        const Transform& getTransform(const FrameId& a, const FrameId& b) const;
+        const Transform getTransform(const FrameId& a, const FrameId& b) const;
 
         /** @return the edge between frame @p origin and @p target
          * @throw UnknownTransformException if there is no such edge  */
--- src/TransformGraphVisitors.hpp
+++ src/TransformGraphVisitors.hpp
@@ -6,40 +6,80 @@
  */
 
 #pragma once
+#include "TransformGraphExceptions.hpp"
+#include <boost/shared_ptr.hpp>
 #include <boost/graph/breadth_first_search.hpp>
+
+#include <deque>
+#include <unordered_map>
 
 namespace envire { namespace core
 {
 
-    template < typename Vertex >
+    class Empty{};
+
+    template < typename Vertex>
     class TransformGraphBFSVisitor: public boost::default_bfs_visitor
     {
     public:
         template <typename Graph>
-        TransformGraphBFSVisitor(Vertex &v, Graph &g):vertex_to_origin(v)
-        {parent.resize(g.num_vertices());}
+        TransformGraphBFSVisitor(const Vertex &v,const Graph &g):vertex_to_search(v)
+        {
+            parent.reset(new std::unordered_map<Vertex,Vertex>());
+            tree.reset(new std::deque<Vertex>());
+        }
 
         template <typename Graph>
-        void examine_vertex(Vertex v, const Graph &g)
+        void discover_vertex(Vertex v, const Graph &g)
         {
-            std::cout<<"EXAMINE["<<g[v].frame.name<<"]\n";
+            if (vertex_to_search == v)
+            {
+                Vertex parent_vertex = v;
+
+                /** Get its direct parent **/
+                while (parent_vertex != Graph::null_vertex())
+                {
+                    tree->push_front(parent_vertex);
+                    try
+                    {
+                        parent_vertex = parent->at(parent_vertex);
+                    }catch (const std::out_of_range& oor)
+                    {
+                        parent_vertex = Graph::null_vertex();
+                    }
+                }
+
+                throw FoundFrameException(g[v].frame.name);
+            }
         }
+
 
         template < typename Edge, typename Graph>
         void tree_edge(Edge e, const Graph &g)
         {
             Vertex source_vertex = boost::source(e, g);
             Vertex target_vertex = boost::target(e, g);
-            //parent[target_vertex] = source_vertex;
-
-            std::cout<<"EDGE"<<g[source_vertex].frame.name<<"->"<<g[target_vertex].frame.name<<"\n";
+            parent->insert(std::make_pair<Vertex,Vertex>(static_cast<Vertex>(target_vertex), static_cast<Vertex>(source_vertex)));
         }
 
 
 
     public:
-        Vertex vertex_to_origin;
-        std::vector<Vertex> parent;
+        Vertex vertex_to_search;
+        boost::shared_ptr< std::unordered_map<Vertex, Vertex> > parent;
+        boost::shared_ptr< std::deque<Vertex> > tree; /** tree from origin tree[0] to target tree[n] */
     };
+
+
+    /** TESTING BOOST CODE **/
+    template < class Visitor=boost::null_visitor>
+    class TransformGraphBFSRecorderVisitor: public boost::bfs_visitor<Visitor>
+    {
+    public:
+        TransformGraphBFSRecorderVisitor():boost::bfs_visitor<>(){ }
+        TransformGraphBFSRecorderVisitor(Visitor record_vis):boost::bfs_visitor<Visitor>(record_vis){ }
+
+    };
+
 }}
 
