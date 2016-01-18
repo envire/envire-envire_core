28b002e8563c3c5f38f65b182b4921f437862afe
Sascha Arnold <sascha.arnold@dfki.de>
2016-01-18 14:00:46 +0100

    added missing return values in copy and move operators

--- src/items/ItemBase.cpp
+++ src/items/ItemBase.cpp
@@ -24,6 +24,7 @@ ItemBase& ItemBase::operator=(const ItemBase& item)
     time = item.time;
     uuid = item.uuid;
     frame_name = item.frame_name;
+    return *this;
 }
 
 ItemBase& ItemBase::operator=(ItemBase&& item)
@@ -31,4 +32,5 @@ ItemBase& ItemBase::operator=(ItemBase&& item)
     time = std::move(item.time);
     uuid = std::move(item.uuid);
     frame_name = std::move(item.frame_name);
+    return *this;
 }
