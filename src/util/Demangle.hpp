cbbccd440aff8c26dc14c69dbcdf7cfb927a49c3
Arne Böckmann <arne.boeckmann@dfki.de>
2016-01-25 14:21:46 +0100

    added demangle header

---
+++ src/util/Demangle.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <string>
+#include <cxxabi.h>
+
+namespace envire { namespace core { 
+  
+std::string demangleTypeName(const std::type_index& type)
+{
+  int status = 0;
+  std::string result;
+  char* p_nice_name = abi::__cxa_demangle(type.name(), NULL, NULL, &status);
+  if(0 != status) 
+  {
+      //if demangling failed return the mangled name
+      result = type.name();
+  }
+  else
+  {
+      if(nullptr != p_nice_name)
+      {
+          result = p_nice_name;
+          free(p_nice_name);
+      }
+
+  }
+  return result;  
+}
+
+}}
