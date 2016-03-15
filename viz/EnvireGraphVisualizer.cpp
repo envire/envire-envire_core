cda96a9baebd2fa2212a9165f24380817eb5410e
Arne Böckmann <arne.boeckmann@dfki.de>
2016-03-15 15:58:41 +0100

    First version of EnvireVisualizer

---
+++ viz/EnvireGraphVisualizer.cpp
@@ -0,0 +1,107 @@
+#include "EnvireGraphVisualizer.hpp"
+#include <envire_core/items/Transform.hpp>
+#include <envire_core/graph/EnvireGraph.hpp>
+#include <string>
+
+using namespace envire::core;
+using namespace vizkit3d;
+
+
+#define ASSERT_NOT_NULL(item) \
+  do \
+  { \
+    if(item == nullptr) \
+    { \
+      throw std::runtime_error(std::string(#item) + " is null"); \
+    } \
+  }while(false)
+
+namespace envire { namespace viz 
+{
+  
+
+  
+EnvireGraphVisualizer::EnvireGraphVisualizer(EnvireGraph& graph,
+                                             Vizkit3DWidget* widget, 
+                                             const FrameId& rootNode) :
+  graph(graph), widget(widget)
+{ 
+  auto edgeLambda = std::bind(&EnvireGraphVisualizer::edgeAddedToTree, this,
+                              std::placeholders::_1, std::placeholders::_2);
+
+  tree.edgeAdded.connect(edgeLambda);
+  graph.getTree(rootNode, true, &tree);
+  widget->setRootFrame(QString::fromStdString(rootNode));
+}
+
+void EnvireGraphVisualizer::edgeAddedToTree(vertex_descriptor origin, vertex_descriptor target)
+{
+    const Transform tf = graph.getTransform(origin, target);
+    QQuaternion rot;
+    QVector3D trans;
+    std::tie(rot, trans)= convertTransform(tf);
+    const QString qSrc = QString::fromStdString(graph.getFrameId(origin));
+    const QString qTarget = QString::fromStdString(graph.getFrameId(target));
+    widget->setTransformation(qSrc, qTarget, trans, rot);
+    loadItems(target);
+}
+
+
+std::pair<QQuaternion, QVector3D> EnvireGraphVisualizer::convertTransform(const Transform& tf) const
+{
+  //normalizing is important, otherwise osg will break when switching the root.
+  const base::Quaterniond& rot = tf.transform.orientation.normalized();
+  const base::Position& pos = tf.transform.translation;
+  const QQuaternion orientation(rot.w(), rot.x(), rot.y(), rot.z());
+  const QVector3D translation(pos.x(), pos.y(), pos.z());
+  return std::make_pair(orientation, translation);
+}
+
+void EnvireGraphVisualizer::loadItems(const vertex_descriptor vertex)
+{
+  //items are loaded using the qt mechanism that was originally developed for ruby
+  //interaction. Thus most of the methods need to be invoked.
+  
+  //FIXME ausführlich dokumentieren
+  for (const auto& it : pluginsNames)
+  {
+    const std::type_index type = it.first;
+    const QString pluginName = it.second;
+    
+    std::cout << "looking for " << type.name() << std::endl;
+    
+    if(!graph.containsItems(vertex, type))
+      continue;
+    
+    const Frame::ItemList& items = graph.getItems(vertex, type);
+    for(ItemBase::Ptr item : items)
+    { 
+      //FIXME hier später nicht immer neue plugins erzeugen
+      QObject* plugin = widget->loadPlugin("", pluginName);
+      ASSERT_NOT_NULL(plugin);
+      VizPluginBase* vizPlugin = dynamic_cast<VizPluginBase*>(plugin);
+      ASSERT_NOT_NULL(vizPlugin);
+      std::cout << "AAAAAAAAAAAAAAAAAAAAAa" << std::endl;
+      vizPlugin->updateDataRaw(item->getRawData(), item->getEmbeddedTypeInfo());
+    }
+  }
+}
+
+void EnvireGraphVisualizer::addPluginInfo(std::type_index type, const QString& pluginName)
+{
+  if(pluginsNames.find(type) != pluginsNames.end())
+  {
+    //FIXME better error message
+    throw std::runtime_error("Plugin for type already registered");
+  }
+  pluginsNames[type] = pluginName;
+  
+  //whenever a plugin is added, we need to revisit the whole tree and check if 
+  //FIXME hier sollte das nur für den typ gemacht werden
+  tree.visitDfs(tree.root, [&] (vertex_descriptor node, vertex_descriptor parent)
+  {
+    loadItems(node);
+  });
+}
+
+}}
---
+++ viz/EnvireGraphVisualizer.hpp
@@ -0,0 +1,53 @@
+#pragma once
+#include <unordered_map>
+#include <typeindex>
+#include <vizkit3d/Vizkit3DPlugin.hpp>
+#include <vizkit3d/Vizkit3DWidget.hpp>
+#include <envire_core/graph/GraphTypes.hpp>
+#include <envire_core/graph/TreeView.hpp>
+
+namespace envire { namespace core 
+{
+  class Transform;
+  class EnvireGraph;
+}}
+
+namespace envire { namespace viz
+{
+
+class EnvireGraphVisualizer
+{
+  using vertex_descriptor = envire::core::GraphTraits::vertex_descriptor;
+  using edge_descriptor = envire::core::GraphTraits::edge_descriptor;
+public:
+  
+  /**
+   * @param rootNode the name of the frame that should be placed in the world origin*/
+  EnvireGraphVisualizer(envire::core::EnvireGraph& graph,
+                        vizkit3d::Vizkit3DWidget* widget, 
+                        const envire::core::FrameId& rootNode);
+  void addPluginInfo(std::type_index type, const QString& pluginName);
+  
+private:
+  void loadPlugins();
+  
+  /**event handler*/
+  void edgeAddedToTree(vertex_descriptor origin, vertex_descriptor target);
+  
+  /**Display all items that are in @p vertex */
+  void loadItems(const vertex_descriptor vertex);
+   
+  
+  
+  
+  std::pair<QQuaternion, QVector3D> convertTransform(const envire::core::Transform& tf) const;
+  
+  
+  /**Mapping from item type to the name of the plugin that can display this type*/
+  std::unordered_map<std::type_index, QString> pluginsNames;
+  envire::core::EnvireGraph& graph; /**< the graph that is visualized*/
+  envire::core::TreeView tree;
+  vizkit3d::Vizkit3DWidget* widget;
+};
+
+}}
