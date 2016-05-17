921c9753380ee3fa9a34d0b7ec13b93fbb39e80c
Arne Böckmann <arne.boeckmann@dfki.de>
2016-05-17 10:51:22 +0200

    viz: cleanup

--- viz/EnvireVisualizer.cpp
+++ viz/EnvireVisualizer.cpp
@@ -25,109 +25,6 @@ using namespace envire::core;
 using namespace vizkit3d;
 using namespace envire::viz;
 
-// int testWindow(int argc, char** argv)
-// {
-//   EnvireGraph graph;
-//   graph->addFrame("root");
-//   
-//   QApplication app(argc, argv);
-//   MainWindow window(graph, "root");
-//   window.show();
-//   
-//   
-//   return app.exec();
-// }
-
-// /** minimal usage example for the viszualizer*/
-// void minimalExample(EnvireGraph& graph)
-// {
-//   /
-//   QtThreadedWidget<Vizkit3DWidget> app;
-//   app.start();
-//   
-//   Vizkit3DWidget* widget = dynamic_cast<Vizkit3DWidget*>(app.getWidget());
-//   
-//   //gather information about all available vizkit3d plugins
-//   envire::viz::Vizkit3dPluginInformation info(widget);
-//   
-//   //note: the graph has to contain a node called "rootNode".
-//   envire::viz::EnvireGraphVisualizer visualizer(graph, widget, "rootNode", info);
-//   
-//   while(true)
-//   {
-//     //keep the program running
-//   }  
-// }
-// 
-// int testThreaded(int argc, char** argv)
-// {
-//   plugin_manager::PluginLoader* loader = plugin_manager::PluginLoader::getInstance();
-//   envire::core::ItemBase::Ptr cloudItem;
-//   envire::core::ItemBase::Ptr cloudItem2;
-//   envire::core::ItemBase::Ptr cloudItem3;
-//   loader->createInstance("envire::pcl::PointCloud", cloudItem);
-//   loader->createInstance("envire::pcl::PointCloud", cloudItem2);
-//   loader->createInstance("envire::pcl::PointCloud", cloudItem3);
-//   envire::pcl::PointCloud::Ptr cloud = boost::dynamic_pointer_cast<envire::pcl::PointCloud>(cloudItem);
-//   envire::pcl::PointCloud::Ptr cloud2 = boost::dynamic_pointer_cast<envire::pcl::PointCloud>(cloudItem2);
-//   envire::pcl::PointCloud::Ptr cloud3 = boost::dynamic_pointer_cast<envire::pcl::PointCloud>(cloudItem3);
-//   pcl::PCDReader reader;
-//   reader.read("/home/arne/git/rock-entern/slam/pcl/test/bunny.pcd", cloud->getData());
-//   reader.read("/home/arne/git/rock-entern/slam/pcl/test/bunny.pcd", cloud2->getData());
-//   reader.read("/home/arne/git/rock-entern/slam/pcl/test/cturtle.pcd", cloud3->getData());
-//   
-//   EnvireGraph graph;
-//   graph->addFrame("A"); 
-//   graph->addFrame("B");
-//   graph->addFrame("C");
-//   graph->addItemToFrame("A", cloud);
-//   graph->addItemToFrame("B", cloud2);
-//   graph->addItemToFrame("C", cloud3); //special case item in root node
-//   
-//   Transform ab(base::Position(1, 1, 1), Eigen::Quaterniond (Eigen::AngleAxisd(0.5, Eigen::Vector3d(1,2,3))));
-//   graph->addTransform("A", "B", ab);
-//   Transform bc(base::Position(1, 0, 0.3), Eigen::Quaterniond(Eigen::AngleAxisd(0.3, Eigen::Vector3d(1,0,3))));
-//   graph->addTransform("B", "C", ab);  
-//   
-//   QtThreadedWidget<Vizkit3DWidget> app;
-//   app.start();
-//   Vizkit3DWidget* widget = dynamic_cast<Vizkit3DWidget*>(app.getWidget());
-//   envire::viz::Vizkit3dPluginInformation info(widget);
-//   envire::viz::EnvireGraphVisualizer visualizer(graph, widget, "A", info);
-//   http://www.oldtimer-markt.de/sites/default/files/otm_versicherungsvergleich_2015_0.pdf
-//      graph->addTransform("D", "E", ab);
-//      graph->addTransform("D", "F", bc);
-//      graph->addTransform("C", "D", ab);//should add the whole sub-tree at once
-//    
-//   while(true)
-//   {
-//     
-//     std::this_thread::sleep_for(std::chrono::milliseconds(200));
-//     ab = graph->getTransform("A", "B");
-//     ab.transform.orientation *= base::Quaterniond(Eigen::AngleAxisd(0.23, base::Position(1, 1, 0)));
-//     graph->updateTransform("A", "B", ab);
-//     
-//     if(graph->containsEdge("B", "C"))
-//     {
-//       graph->removeTransform("B", "C");
-//     }
-//     else
-//     {
-//       graph->addTransform("B", "C", ab);
-//     }
-//     
-//     if(graph->containsEdge("C", "D"))
-//     {
-//       graph->removeTransform("C", "D");
-//     }
-//     else
-//     {
-//       graph->addTransform("C", "D", ab);
-//     }   
-//   }
-//   return 0;
-// }
-
 int test(int argc, char **argv)
 {
   
@@ -175,37 +72,35 @@ int test(int argc, char **argv)
   window.displayGraph("envire_graph_test23");
   window.show();
   
+  std::shared_ptr<EnvireGraph> loadedGraph(window.getGraph());
   
-//   std::thread t([&]() 
-//   {
-//     //because the graph is not thread safe, yet  
-//     std::this_thread::sleep_for(std::chrono::seconds(1));
-//     
-//     std::vector<FrameId> randTreeNodes;
-//     std::vector<ItemBase::Ptr> randTreeItems;
-//     randTreeNodes.push_back("randTree");
-//     boost::uuids::random_generator generator;
-//     int i = 0;
-//     bool removeSubTree = false;
-//     
-//     Transform tf;
-//     tf.transform.orientation = base::Quaterniond(Eigen::AngleAxisd(0.0, base::Position(0, 0, 1)));
-//     tf.transform.translation << 1,0,1;
-//     graph->addTransform("sub2", "sub4", tf); //part of the sub tree that always remains
-//     
-//     while(true)
-//     {
-//       ++i;
-//       std::this_thread::sleep_for(std::chrono::milliseconds(100));
-//       
-//       //rotate transformation
-//       tf = graph->getTransform("A", "B");
-//       tf.transform.orientation *= base::Quaterniond(Eigen::AngleAxisd(0.23, base::Position(1, 0, 0)));
-//       graph->updateTransform("A", "B", tf);
-//       
-//       
+  
+  std::thread t([&]() 
+  {
+    //because the graph is not thread safe, yet  
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    
+    std::vector<FrameId> randTreeNodes;
+    std::vector<ItemBase::Ptr> randTreeItems;
+    randTreeNodes.push_back("randTree");
+    boost::uuids::random_generator generator;
+    int i = 0;
+    bool removeSubTree = false;
+    
+    Transform tf;
+    while(true)
+    {
+      ++i;
+      std::this_thread::sleep_for(std::chrono::milliseconds(100));
+      
+      //rotate transformation
+      tf = loadedGraph->getTransform("A", "B");
+      tf.transform.orientation *= base::Quaterniond(Eigen::AngleAxisd(0.23, base::Position(1, 0, 0)));
+      loadedGraph->updateTransform("A", "B", tf);
+      
+      
 //       //change length of transformation
-//    /*   tf = graph->getTransform("C", "D");
+//       tf = graph.getTransform("C", "D");
 //       if(tf.transform.translation.norm() >= 10)
 //       {
 //         tf.transform.translation << 0, 1, 0;
@@ -214,21 +109,21 @@ int test(int argc, char **argv)
 //       {
 //         tf.transform.translation.x() += 0.1;
 //       }
-//       graph->updateTransform("C", "D", tf);
-//       */
+//       graph.updateTransform("C", "D", tf);
+      
 //       //random growing tree
 //       if((i % 10) == 0)
 //       {
 //         const int elem = rand() % randTreeNodes.size();
 //         boost::uuids::uuid id = generator();
 //         const FrameId nextName = boost::uuids::to_string(id);
-//         graph->addFrame(nextName);
+//         graph.addFrame(nextName);
 //         tf.transform.orientation = base::Quaterniond(Eigen::AngleAxisd(0.0, base::Position(0, 0, 1)));
 //         tf.transform.translation << (rand() % 40) / 20.0, (rand() % 40) / 20.0, 0;
-//         graph->addTransform(randTreeNodes[elem], nextName, tf);
+//         graph.addTransform(randTreeNodes[elem], nextName, tf);
 //         randTreeNodes.push_back(nextName);
 //       }
-//       
+      
 //       //randomly add items to the growing tree
 //       if((i % 20) == 0)
 //       {
@@ -239,7 +134,7 @@ int test(int argc, char **argv)
 //         if(reader.read("/home/arne/git/rock-entern/slam/pcl/test/bunny.pcd", milk->getData()) == 0)
 //         {
 //           const int elem = rand() % randTreeNodes.size();
-//           graph->addItemToFrame(randTreeNodes[elem], milk);
+//           graph.addItemToFrame(randTreeNodes[elem], milk);
 //           randTreeItems.push_back(milk);
 //         }
 //       }
@@ -247,51 +142,51 @@ int test(int argc, char **argv)
 //       if((i % 30) == 0 && randTreeItems.size() > 0)
 //       {
 //         LOG(INFO) << "removing item from frame " << randTreeItems.back()->getFrame();
-//         graph->removeItemFromFrame(randTreeItems.back());
+//         graph.removeItemFromFrame(randTreeItems.back());
 //         randTreeItems.pop_back();
 //       } 
 //       
 //       
 //       if((i % 12 == 0))
 //       {
-//         if(graph->getTotalItemCount("A") > 0)
+//         if(graph.getTotalItemCount("A") > 0)
 //         {
-//           graph->clearFrame("A");
+//           graph.clearFrame("A");
 //         }
 //         else
 //         {
-//           graph->addItemToFrame("A", cloud3);
+//           graph.addItemToFrame("A", cloud3);
 //         }
 //       }
-//       /*
-//       // add a sub tree
-//       if((i % 10) == 0) 
-//       {
-//         if(!removeSubTree)
-//         {
-//           tf.transform.orientation = base::Quaterniond(Eigen::AngleAxisd(0.0, base::Position(0, 0, 1)));
-//           tf.transform.translation << 3,4,5;
-//           graph->addTransform("A", "sub1", tf);
-//           tf.transform.orientation = base::Quaterniond(Eigen::AngleAxisd(0.0, base::Position(0, 0, 1)));
-//           tf.transform.translation << 1,0,2;
-//           graph->addTransform("sub1", "sub2", tf);
-//           tf.transform.translation << 0,1,2;
-//           graph->addTransform("sub1", "sub3", tf);
-//           removeSubTree = true;
-//         }
-//         else
-//         {
-//           graph->remove_edge("sub1", "sub2");
-//           graph->remove_edge("sub1", "sub3");
-//           graph->remove_edge("A", "sub1");
-//           removeSubTree = false;
-//         } 
-//         } */
-//     
-//     }
-//   });
+      /*
+      // add a sub tree
+      if((i % 10) == 0) 
+      {
+        if(!removeSubTree)
+        {
+          tf.transform.orientation = base::Quaterniond(Eigen::AngleAxisd(0.0, base::Position(0, 0, 1)));
+          tf.transform.translation << 3,4,5;
+          graph->addTransform("A", "sub1", tf);
+          tf.transform.orientation = base::Quaterniond(Eigen::AngleAxisd(0.0, base::Position(0, 0, 1)));
+          tf.transform.translation << 1,0,2;
+          graph->addTransform("sub1", "sub2", tf);
+          tf.transform.translation << 0,1,2;
+          graph->addTransform("sub1", "sub3", tf);
+          removeSubTree = true;
+        }
+        else
+        {
+          graph->remove_edge("sub1", "sub2");
+          graph->remove_edge("sub1", "sub3");
+          graph->remove_edge("A", "sub1");
+          removeSubTree = false;
+        } 
+        } */
+    
+    }
+  });
   app.exec(); 
-//t.join();
+  t.join();
   return 0;
 }
 
