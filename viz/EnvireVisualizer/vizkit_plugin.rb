05e953cf97e902a3d528b280127292cfa0d8d81b
Arne Böckmann <arne.boeckmann@dfki.de>
2016-06-08 11:08:14 +0200

    viz: moved envire visualizer into its own rock component.

--- viz/EnvireVisualizer/vizkit_plugin.rb
+++
@@ -1,2 +0,0 @@
-Vizkit::UiLoader.register_3d_plugin('EnvireGraphStructureVisualization', 'envire_core', 'EnvireGraphStructureVisualization')
-Vizkit::UiLoader.register_3d_plugin_for('EnvireGraphStructureVisualization', "/envire/core/EnvireGraph", :updateData )
