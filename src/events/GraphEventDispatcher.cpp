97c1a951b75dd8edbdcc7f01dfb30b267a986213
Steffen Planthaber <Steffen.Planthaber@dfki.de>
2019-04-30 10:54:52 +0200

    ability to disable a GraphEventDispatcher temporarily

--- src/events/GraphEventDispatcher.cpp
+++ src/events/GraphEventDispatcher.cpp
@@ -35,7 +35,7 @@
 
 using namespace envire::core;
 
-GraphEventDispatcher::GraphEventDispatcher(GraphEventPublisher* pPublisher): GraphEventSubscriber(pPublisher)
+GraphEventDispatcher::GraphEventDispatcher(GraphEventPublisher* pPublisher): GraphEventSubscriber(pPublisher),enabled(true)
 {}
 
 GraphEventDispatcher::GraphEventDispatcher()
@@ -43,33 +43,37 @@ GraphEventDispatcher::GraphEventDispatcher()
 
 void GraphEventDispatcher::notifyGraphEvent(const GraphEvent& event)
 {
-    switch(event.getType())
+
+    if (enabled) 
     {
-    case GraphEvent::EDGE_ADDED:
-        edgeAdded(dynamic_cast<const EdgeAddedEvent&>(event));
+        switch(event.getType())
+        {
+        case GraphEvent::EDGE_ADDED:
+            edgeAdded(dynamic_cast<const EdgeAddedEvent&>(event));
+            break;
+        case GraphEvent::EDGE_MODIFIED:
+            edgeModified(dynamic_cast<const EdgeModifiedEvent&>(event));
+            break;
+        case GraphEvent::EDGE_REMOVED:
+            edgeRemoved(dynamic_cast<const EdgeRemovedEvent&>(event));
+            break;
+        case GraphEvent::FRAME_ADDED:
+            frameAdded(dynamic_cast<const FrameAddedEvent&>(event));
+            break;
+        case GraphEvent::FRAME_REMOVED:
+            frameRemoved(dynamic_cast<const FrameRemovedEvent&>(event));
+            break;
+        case GraphEvent::ITEM_ADDED_TO_FRAME:
+            itemAdded(dynamic_cast<const ItemAddedEvent&>(event));
+            break;
+        case GraphEvent::ITEM_REMOVED_FROM_FRAME:
+            itemRemoved(dynamic_cast<const ItemRemovedEvent&>(event));
+            break;
+        default:
         break;
-    case GraphEvent::EDGE_MODIFIED:
-        edgeModified(dynamic_cast<const EdgeModifiedEvent&>(event));
-        break;
-    case GraphEvent::EDGE_REMOVED:
-        edgeRemoved(dynamic_cast<const EdgeRemovedEvent&>(event));
-        break;
-    case GraphEvent::FRAME_ADDED:
-        frameAdded(dynamic_cast<const FrameAddedEvent&>(event));
-        break;
-    case GraphEvent::FRAME_REMOVED:
-        frameRemoved(dynamic_cast<const FrameRemovedEvent&>(event));
-        break;
-    case GraphEvent::ITEM_ADDED_TO_FRAME:
-        itemAdded(dynamic_cast<const ItemAddedEvent&>(event));
-        break;
-    case GraphEvent::ITEM_REMOVED_FROM_FRAME:
-        itemRemoved(dynamic_cast<const ItemRemovedEvent&>(event));
-        break;
-    default:
-      break;
-    //no default case because we only handle basic event types here. Item events are handled
-    //by a different class
+        //no default case because we only handle basic event types here. Item events are handled
+        //by a different class
+        }
     }
 }
 
