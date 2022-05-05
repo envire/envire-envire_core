a852c4e7e2debf26ea7faa9c4b216f4c7ed20ad1
Steffen Planthaber <steffen.planthaber@dfki.de>
2022-05-05 11:00:27 +0200

    set env variables for externals inside CMaleLists.txt

--- external_dependencies/build.bash
+++ external_dependencies/build.bash
@@ -5,20 +5,25 @@ if [ "$#" -ne 1 ]; then
     echo "Please specify a prefix. If you want system wide installation use '/usr/local' as prefix. for a local install use e.g. './install'"
     echo "In case you use a local prefix, please source the generated env.sh script in this folder to set up the paths before building the main library"
     echo
-    exit -1  
+    exit -1
 fi
 
-# get global path
-ABS_PREFIX=`readlink -f $1`
-mkdir -p $ABS_PREFIX
-
-# Setup environemtn variables
-export CMAKE_PREFIX_PATH=$ABS_PREFIX/share/rock/cmake/
-export PKG_CONFIG_PATH=$ABS_PREFIX/lib/pkgconfig/
+if [[ $1 != .* && $1 != /* ]]; then
+    echo
+    echo -e "\033[1;33mPlease specify a global path or a local path with '/' or './' \033[1;0m"
+    echo
+    exit -1
+fi
 
 #download and build (in subshell to return to original folder)
   
 mkdir -p build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=$ABS_PREFIX ..
+cmake -DCMAKE_INSTALL_PREFIX=$1 ..
 make -j
+
+if [[ $1 = .* ]]; then
+    echo
+    echo -e "\033[1;33m'source env.sh' in this folder before building the main library \033[1;0m"
+    echo
+fi
