cbe5a266d1e091e33643af8eb35003ff6901f7cc
Steffen Planthaber <steffen.planthaber@dfki.de>
2025-03-26 17:42:02 +0100

    don't touch shared_ptr on recreate

--- src/util/EnvireManager.cpp
+++ src/util/EnvireManager.cpp
@@ -26,11 +26,12 @@ EnvireManager::~EnvireManager() {}
 
 EnvireGraphPtr EnvireManager::getInstance(std::string name, bool force_recreate) {
     EnvireGraphPtr graph = graphs[name];
-    if (!graph.get() || force_recreate){
+    if (!graph.get()){
         //create
         graph = EnvireGraphPtr(new ThreadSaveEnvireGraph());
         graphs[name] = graph;
-
+    } else if (force_recreate) {
+        graph.reset(new ThreadSaveEnvireGraph());
     }
     return graph;
 }
